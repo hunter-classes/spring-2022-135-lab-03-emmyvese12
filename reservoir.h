51685fe891ee3cedcbd624794422f155af006fd2
Emmy Veselinov <emmyveselinov@gmail.com>
2022-02-15 23:07:21 -0500

    added signatures for functions in task B

--- reservoir.h
+++ reservoir.h
@@ -1,5 +1,9 @@
-//signature for the get_east_storage function
+//signatures for each function in reservoir.cpp
 #include <string>
 #pragma once
 
 double get_east_storage(std::string date);
+
+double get_min_east();
+
+double get_max_east();
