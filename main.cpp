8334ac68cb2d825fe952dcd812c3c250487174a2
Emmy Veselinov <emmyveselinov@gmail.com>
2022-02-15 21:41:35 -0500

    added task A

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
-// add your code as appropriate 
 #include <iostream>
+#include "reservoir.h"
 
-int main()
-{
-  
+int main(){
+  std::cout << "Date: " << "04/22/2018" << std::endl;
+  std::cout << "East basin storage: " << get_east_storage("04/22/2018") << std::endl;
 
-  
+  std::cout << "\nDate: " << "12/25/2018" << std::endl;
+  std::cout << "East basin storage: " << get_east_storage("12/25/2018") << std::endl;
+
   return 0;
 }
