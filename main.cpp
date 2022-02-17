8bcc80cdf9f155dfce7dcdbd4b0eb4ac2e4516c1
Emmy Veselinov <emmyveselinov@gmail.com>
2022-02-16 23:07:23 -0500

    added task D

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
+/*
+Emmy Veselinov
+CSCI 135 Lab-03
+
+Task A: Get East Storage Value
+Task B: Minimum and Maximum Storage in 2018
+Task C: Comparing Elevations East and West
+Task D: Reverse Chronological Order
+*/
+
 #include <iostream>
 #include "reservoir.h"
+#include "reverseorder.h"
 
 int main(){
   //task A
   std::cout << "Date: " << "02/22/2018" << std::endl;
   std::cout << "East basin storage: " << get_east_storage("02/22/2018") << " billion gallons" << std::endl;
+
+  std::cout << "\nDate: " << "05/20/2018" << std::endl;
+  std::cout << "East basin storage: " << get_east_storage("05/20/2018") << " billion gallons" << std::endl;
 
   std::cout << "\nDate: " << "12/25/2018" << std::endl;
   std::cout << "East basin storage: " << get_east_storage("12/25/2018") << " billion gallons" << std::endl;
@@ -19,6 +33,13 @@ int main(){
   std::cout << "Which basin is higher on " << "07/09/2018" << "?: " << compare_basins("07/09/2018") << std::endl;
   std::cout << "Which basin is higher on " << "08/05/2018" << "?: " << compare_basins("08/05/2018") << std::endl;
   std::cout << "Which basin is higher on " << "10/11/2018" << "?: " << compare_basins("10/11/2018") << std::endl;
+
+  //task D
+  std::cout << "\nWest basin elevation: " << std::endl;
+  reverse_order("05/29/2018","06/02/2018");
+
+  std::cout << "\nWest basin elevation: " << std::endl;
+  reverse_order("01/05/2018","01/09/2018");
 
   return 0;
 }
